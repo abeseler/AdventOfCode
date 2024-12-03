85ee094fc5b2cd61ad0f66a86e5376d3de774ecb
adam.beseler <adam.beseler@gmail.com>
2024-12-02 23:35:12 -0600

    hamburger

--- AdventOfCode.2024/Day_03/Day_03.cs
+++ AdventOfCode.2024/Day_03/Day_03.cs
@@ -10,10 +10,9 @@ internal static partial class Day_03
         using var reader = new StreamReader(filename);
         while (reader.ReadLine() is { } line)
         {
-            var lineSpan = line.AsSpan();
             foreach (var match in GetMultiplyInstructions().EnumerateMatches(line))
             {
-                var span = lineSpan[(match.Index)..(match.Index + match.Length)];
+                var span = line.AsSpan()[(match.Index)..(match.Index + match.Length)];
                 var commaIndex = span.IndexOf(',');
                 var a = int.Parse(span[4..commaIndex]);
                 var b = int.Parse(span.Slice(commaIndex + 1, span.Length - commaIndex - 2));
