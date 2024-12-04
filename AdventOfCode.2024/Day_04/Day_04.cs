febc544fa6afac88cd573dc6d974d28b4b15772d
adam.beseler <adam.beseler@gmail.com>
2024-12-04 00:34:45 -0600

    day FOUR

---
+++ AdventOfCode.2024/Day_04/Day_04.cs
@@ -0,0 +1,112 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace AdventOfCode2024;
+
+internal static class Day_04
+{
+    public static int Part_1(string filename)
+    {        
+        var map = new Dictionary<Vector2, char>();
+        var pointsToCheck = new List<Vector2>();
+        var row = 0;
+        using var reader = new StreamReader(filename);
+        while (reader.ReadLine() is { } line)
+        {
+            for (var col = 0; col < line.Length; col++)
+            {
+                var point = new Vector2(col, row);
+                var character = line[col];
+                map.Add(point, character);
+                if (character is 'X')
+                {
+                    pointsToCheck.Add(point);
+                }
+            }
+
+            row++;
+        }
+
+        var matches = 0;
+        ReadOnlySpan<char> expected = ['M', 'A', 'S'];
+        ReadOnlySpan<Vector2> directions =
+        [
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(-1, 0),
+            new Vector2(0, -1),
+            new Vector2(1, 1),
+            new Vector2(-1, 1),
+            new Vector2(1, -1),
+            new Vector2(-1, -1),
+        ];
+
+        foreach (var point in pointsToCheck)
+        {
+            foreach (var direction in directions)
+            {
+                var currentPoint = point;
+                var found = true;
+                foreach (var expectedChar in expected)
+                {
+                    currentPoint += direction;
+                    if (!map.TryGetValue(currentPoint, out var character) || character != expectedChar)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    matches++;
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    public static int Part_2(string filename)
+    {
+        var map = new Dictionary<Vector2, char>();
+        var pointsToCheck = new List<Vector2>();
+        var row = 0;
+        using var reader = new StreamReader(filename);
+        while (reader.ReadLine() is { } line)
+        {
+            for (var col = 0; col < line.Length; col++)
+            {
+                var point = new Vector2(col, row);
+                var character = line[col];
+                map.Add(point, character);
+                if (character is 'A')
+                {
+                    pointsToCheck.Add(point);
+                }
+            }
+
+            row++;
+        }
+
+        var matches = 0;
+
+        foreach (var point in pointsToCheck)
+        {
+            if (map.TryGetValue(point + new Vector2(-1, -1), out var upleft)
+                && upleft is 'M' or 'S'
+                && map.TryGetValue(point + new Vector2(1, 1), out var downright)
+                && downright is 'M' or 'S'
+                && upleft != downright
+                && map.TryGetValue(point + new Vector2(1, -1), out var upright)
+                && upright is 'M' or 'S'
+                && map.TryGetValue(point + new Vector2(-1, 1), out var downleft)
+                && downleft is 'M' or 'S'
+                && upright != downleft)
+            {
+                matches++;
+            }
+        }
+
+        return matches;
+    }
+}
--- AdventOfCode.2024/Runner.cs
+++ AdventOfCode.2024/Runner.cs
@@ -6,17 +6,24 @@ internal static class Runner
     {
         //BenchmarkRunner.Run<Benchmark>();
 
-        Logger.MeasureAndLog(Day_01.Part_1, "Day_01/Example.txt", 1, true);
-        Logger.MeasureAndLog(Day_01.Part_1, "Day_01/Input.txt", 1, false);
-        Logger.MeasureAndLog(Day_01.Part_2, "Day_01/Example.txt", 1, true);
-        Logger.MeasureAndLog(Day_01.Part_2, "Day_01/Input.txt", 1, false);
-        Logger.MeasureAndLog(Day_02.Part_1, "Day_02/Example.txt", 1, true);
-        Logger.MeasureAndLog(Day_02.Part_1, "Day_02/Input.txt", 1, false);
-        Logger.MeasureAndLog(Day_02.Part_2, "Day_02/Example.txt", 1, true);
-        Logger.MeasureAndLog(Day_02.Part_2, "Day_02/Input.txt", 1, false);
-        Logger.MeasureAndLog(Day_03.Part_1, "Day_03/Example.txt", 1, true);
-        Logger.MeasureAndLog(Day_03.Part_1, "Day_03/Input.txt", 1, false);
-        Logger.MeasureAndLog(Day_03.Part_2, "Day_03/Example2.txt", 1, true);
-        Logger.MeasureAndLog(Day_03.Part_2, "Day_03/Input.txt", 1, false);
+        //Logger.MeasureAndLog(Day_01.Part_1, "Day_01/Example.txt", 1, false);
+        //Logger.MeasureAndLog(Day_01.Part_1, "Day_01/Input.txt", 1, false);
+        //Logger.MeasureAndLog(Day_01.Part_2, "Day_01/Example.txt", 1, false);
+        //Logger.MeasureAndLog(Day_01.Part_2, "Day_01/Input.txt", 1, false);
+        
+        //Logger.MeasureAndLog(Day_02.Part_1, "Day_02/Example.txt", 1, false);
+        //Logger.MeasureAndLog(Day_02.Part_1, "Day_02/Input.txt", 1, false);
+        //Logger.MeasureAndLog(Day_02.Part_2, "Day_02/Example.txt", 1, false);
+        //Logger.MeasureAndLog(Day_02.Part_2, "Day_02/Input.txt", 1, false);
+        
+        //Logger.MeasureAndLog(Day_03.Part_1, "Day_03/Example.txt", 1, false);
+        //Logger.MeasureAndLog(Day_03.Part_1, "Day_03/Input.txt", 1, false);
+        //Logger.MeasureAndLog(Day_03.Part_2, "Day_03/Example2.txt", 1, false);
+        //Logger.MeasureAndLog(Day_03.Part_2, "Day_03/Input.txt", 1, false);
+        
+        Logger.MeasureAndLog(Day_04.Part_1, "Day_04/Example.txt", 1, false);
+        Logger.MeasureAndLog(Day_04.Part_1, "Day_04/Input.txt", 1, false);
+        Logger.MeasureAndLog(Day_04.Part_2, "Day_04/Example.txt", 1, false);
+        Logger.MeasureAndLog(Day_04.Part_2, "Day_04/Input.txt", 1, false);
     }
 }
