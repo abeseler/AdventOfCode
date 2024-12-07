20389ca4eedb490ba5af9d966fb9a537d16ec425
adam.beseler <adam.beseler@gmail.com>
2024-12-06 23:53:37 -0600

    day 7

--- 2024/Solutions/Day07_Part1.cs
+++ 2024/Solutions/Day07_Part1.cs
@@ -1,14 +1,57 @@
-﻿namespace AdventOfCode.Solutions;
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Solutions;
 
 internal sealed partial class Day07_Part1 : PuzzleSolution
 {
     private const string DAY = "07";
     public static string FileName { get; } = $"Data/{DAY}.input";
     public static string TestFileName { get; } = $"Data/{DAY}.sample";
-    public static string TestOutputExpected { get; } = "";
+    public static string TestOutputExpected { get; } = "3749";
 
     public static string Solve(StreamReader reader)
     {
-        return "";
+        var operations = new List<(long result, long[] operands)>();
+        while (reader.ReadLine() is { } line)
+        {
+            var resultEnd = line.IndexOf(':');
+            var result = long.Parse(line[..resultEnd]);
+            var operands = line[(resultEnd + 2)..].Split(' ').Select(long.Parse).ToArray();
+            operations.Add((result, operands));
+        }
+
+        var sum = 0L;
+        int[] operators = [];
+
+        foreach (var (result, operands) in operations)
+        {
+            operators = new int[operands.Length - 1];
+
+            for (int i = 0; i < (1 << operators.Length); i++)
+            {
+                for (int j = 0; j < operators.Length; j++)
+                {
+                    operators[j] = (i & (1 << j)) != 0 ? 1 : 0;
+                }
+
+                var current = operands[0];
+                for (int j = 1; j < operands.Length; j++)
+                {
+                    current = operators[j - 1] switch
+                    {
+                        0 => current + operands[j],
+                        1 => current * operands[j],
+                        _ => throw new UnreachableException()
+                    };
+                }
+                if (current == result)
+                {
+                    sum += result;
+                    break;
+                }
+            }
+        }
+
+        return sum.ToString();
     }
 }
--- 2024/Solutions/Day07_Part2.cs
+++ 2024/Solutions/Day07_Part2.cs
@@ -1,14 +1,77 @@
-﻿namespace AdventOfCode.Solutions;
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Solutions;
 
 internal sealed partial class Day07_Part2 : PuzzleSolution
 {
     private const string DAY = "07";
     public static string FileName { get; } = $"Data/{DAY}.input";
     public static string TestFileName { get; } = $"Data/{DAY}.sample";
-    public static string TestOutputExpected { get; } = "";
+    public static string TestOutputExpected { get; } = "11387";
 
     public static string Solve(StreamReader reader)
     {
-        return "";
+        var operations = new List<(long result, long[] operands)>();
+        while (reader.ReadLine() is { } line)
+        {
+            var resultEnd = line.IndexOf(':');
+            var result = long.Parse(line[..resultEnd]);
+            var operands = line[(resultEnd + 2)..].Split(' ').Select(long.Parse).ToArray();
+            operations.Add((result, operands));
+        }
+
+        var sum = 0L;
+        int[] operators = [];
+
+        foreach (var (result, operands) in operations)
+        {
+            operators = new int[operands.Length - 1];
+            var value = GenerateAndTestCombinations(result, operands, operators, 0);
+            sum += value;
+        }
+
+        return sum.ToString();
+    }
+
+    private static long GenerateAndTestCombinations(long result, long[] operands, int[] operators, int position)
+    {
+        if (position == operators.Length)
+        {
+            if (CalculatesToExpected(result, operands, operators))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        for (int i = 0; i < 3; i++) // 3 possibilities: 0, 1, 2
+        {
+            operators[position] = i;
+            var value = GenerateAndTestCombinations(result, operands, operators, position + 1);
+            if (value != 0)
+            {
+                return value;
+            }
+        }
+
+        return 0;
+    }
+
+    private static bool CalculatesToExpected(long result, long[] operands, int[] operators)
+    {
+        var value = operands[0];
+
+        for (int i = 1; i < operands.Length; i++)
+        {
+            value = operators[i - 1] switch
+            {
+                0 => value + operands[i],
+                1 => value * operands[i],
+                2 => long.Parse(value.ToString() + operands[i].ToString()),
+                _ => throw new UnreachableException()
+            };
+        }
+
+        return value == result;
     }
 }
