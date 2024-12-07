8c1fb8c816e8b29f18761c6cd82c7ae67e4dddb8
adam.beseler <adam.beseler@gmail.com>
2024-12-07 01:42:40 -0600

    prep day 8

---
+++ 2024/Solutions/Day08_Part1.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Solutions;
+
+internal sealed class Day08_Part1 : PuzzleSolution
+{
+    private const string DAY = "08";
+    public static string FileName { get; } = $"Data/{DAY}.input";
+    public static string TestFileName { get; } = $"Data/{DAY}.sample";
+    public static string TestOutputExpected { get; } = "3749";
+
+    public static string Solve(StreamReader reader)
+    {
+        while (reader.ReadLine() is { } line)
+        {
+            
+        }
+
+        return "";
+    }
+}
