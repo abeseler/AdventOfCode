62ff56d12957fa482f7c3d816234f359649f0cb5
adam.beseler <adam.beseler@gmail.com>
2024-12-08 01:38:47 -0600

    24 day 8

--- 2024/Solutions/Day08_Part1.cs
+++ 2024/Solutions/Day08_Part1.cs
@@ -1,19 +1,129 @@
-﻿namespace AdventOfCode.Solutions;
+﻿using System.Numerics;
+
+namespace AdventOfCode.Solutions;
 
 internal sealed class Day08_Part1 : PuzzleSolution
 {
     private const string DAY = "08";
     public static string FileName { get; } = $"Data/{DAY}.input";
     public static string TestFileName { get; } = $"Data/{DAY}.sample";
-    public static string TestOutputExpected { get; } = "";
+    public static string TestOutputExpected { get; } = "14";
 
     public static string Solve(StreamReader reader)
     {
+        var antennas = new Dictionary<char, List<Antenna>>();
+
+        var height = 0;
+        var width = 0;
         while (reader.ReadLine() is { } line)
         {
-            
+            for (var col = 0; col < line.Length; col++)
+            {
+                var c = line[col];
+                if (c == '.') continue;
+
+                var antenna = new Antenna(new Vector2(col, height), c);
+                if (antennas.ContainsKey(c) is false)
+                {
+                    antennas[c] = [];
+                }
+                antennas[c].Add(antenna);
+            }
+            width = line.Length;
+            height++;
         }
 
-        return "";
+        var antiNodes = new Dictionary<Vector2, AntiNodes>();
+
+        foreach (var (frequency, antennaList) in antennas)
+        {
+            for (var i = 0; i < antennaList.Count; i++)
+            {
+                for (var j = i + 1; j < antennaList.Count; j++)
+                {
+                    var a = antennaList[i];
+                    var b = antennaList[j];
+                    var positions = CalculateAntiNodePositions(a, b);
+                    foreach (var position in positions)
+                    {
+                        if (position.X < 0 || position.X >= width || position.Y < 0 || position.Y >= height)
+                        {
+                            continue;
+                        }
+                        if (antiNodes.ContainsKey(position) is false)
+                        {
+                            antiNodes[position] = new AntiNodes(position, []);
+                        }
+                        if (antiNodes[position].Frequencies.Contains(frequency) is false)
+                        {
+                            antiNodes[position].Frequencies.Add(frequency);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Console.ForegroundColor = ConsoleColor.Gray;
+        //for (var i = 0; i < height; i++)
+        //{
+        //    for (var j = 0; j < width; j++)
+        //    {
+        //        var position = new Vector2(j, i);
+        //        var antenna = antennas.Values.FirstOrDefault(a => a.Any(a => a.Location == position));
+        //        if (antenna is not null)
+        //        {
+        //            if (antiNodes.ContainsKey(position) && antiNodes[position].IsCancelledOut)
+        //            {
+        //                Console.BackgroundColor = ConsoleColor.DarkRed;
+        //            }
+        //            else if (antiNodes.ContainsKey(position))
+        //            {
+        //                Console.BackgroundColor = ConsoleColor.DarkBlue;
+        //            }
+        //            Console.Write(antenna.First(a => a.Location == position).Frequency);
+        //            Console.ResetColor();
+        //            continue;
+        //        }
+
+        //        if (antiNodes.ContainsKey(position) && antiNodes[position].IsCancelledOut)
+        //        {
+        //            Console.BackgroundColor = ConsoleColor.DarkRed;
+        //            Console.Write('.');
+        //            Console.ResetColor();
+        //        }
+        //        else if (antiNodes.ContainsKey(position))
+        //        {
+        //            Console.BackgroundColor = ConsoleColor.DarkBlue;
+        //            Console.Write('#');
+        //            Console.ResetColor();
+        //        }
+        //        else
+        //        {
+        //            Console.Write('.');
+        //        }
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        return antiNodes.Count.ToString();
+    }
+
+    private static IEnumerable<Vector2> CalculateAntiNodePositions(Antenna a, Antenna b)
+    {
+        var diffBA = b.Location - a.Location;
+        yield return b.Location + diffBA;
+
+        var diffAB = a.Location - b.Location;
+        yield return a.Location + diffAB;
+    }
+
+    private sealed record Antenna(Vector2 Location, char Frequency)
+    {
+
+    }
+
+    private sealed record AntiNodes(Vector2 Location, HashSet<char> Frequencies)
+    {
+        public bool IsCancelledOut => false;
     }
 }
--- 2024/Solutions/Day08_Part2.cs
+++ 2024/Solutions/Day08_Part2.cs
@@ -1,19 +1,147 @@
-﻿namespace AdventOfCode.Solutions;
+﻿using System.Numerics;
+
+namespace AdventOfCode.Solutions;
 
 internal sealed class Day08_Part2 : PuzzleSolution
 {
     private const string DAY = "08";
     public static string FileName { get; } = $"Data/{DAY}.input";
     public static string TestFileName { get; } = $"Data/{DAY}.sample";
-    public static string TestOutputExpected { get; } = "";
+    public static string TestOutputExpected { get; } = "34";
 
     public static string Solve(StreamReader reader)
     {
+        var antennas = new Dictionary<char, List<Antenna>>();
+
+        var height = 0;
+        var width = 0;
         while (reader.ReadLine() is { } line)
         {
+            for (var col = 0; col < line.Length; col++)
+            {
+                var c = line[col];
+                if (c == '.') continue;
 
+                var antenna = new Antenna(new Vector2(col, height), c);
+                if (antennas.ContainsKey(c) is false)
+                {
+                    antennas[c] = [];
+                }
+                antennas[c].Add(antenna);
+            }
+            width = line.Length;
+            height++;
         }
 
-        return "";
+        var antiNodes = new Dictionary<Vector2, AntiNodes>();
+
+        foreach (var (frequency, antennaList) in antennas)
+        {
+            for (var i = 0; i < antennaList.Count; i++)
+            {
+                for (var j = i + 1; j < antennaList.Count; j++)
+                {
+                    var a = antennaList[i];
+                    var b = antennaList[j];
+                    var positions = CalculateAntiNodePositions(a, b, new(width, height));
+                    foreach (var position in positions)
+                    {
+                        if (position.X < 0 || position.X >= width || position.Y < 0 || position.Y >= height)
+                        {
+                            continue;
+                        }
+                        if (antiNodes.ContainsKey(position) is false)
+                        {
+                            antiNodes[position] = new AntiNodes(position, []);
+                        }
+                        if (antiNodes[position].Frequencies.Contains(frequency) is false)
+                        {
+                            antiNodes[position].Frequencies.Add(frequency);
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                var position = new Vector2(j, i);
+                var antenna = antennas.Values.FirstOrDefault(a => a.Any(a => a.Location == position));
+                if (antenna is not null)
+                {
+                    if (antiNodes.ContainsKey(position) && antiNodes[position].IsCancelledOut)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                    }
+                    else if (antiNodes.ContainsKey(position))
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    }
+                    Console.Write(antenna.First(a => a.Location == position).Frequency);
+                    Console.ResetColor();
+                    continue;
+                }
+
+                if (antiNodes.ContainsKey(position) && antiNodes[position].IsCancelledOut)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Write('.');
+                    Console.ResetColor();
+                }
+                else if (antiNodes.ContainsKey(position))
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.Write('#');
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+
+        var locations = antiNodes.Keys.ToHashSet();
+        foreach (var antennaList in antennas.Values)
+        {
+            foreach (var antenna in antennaList)
+            {
+                locations.Add(antenna.Location);
+            }
+        }
+
+        return locations.Count.ToString();
+    }
+
+    private static IEnumerable<Vector2> CalculateAntiNodePositions(Antenna a, Antenna b, Vector2 bounds)
+    {
+        var step = b.Location - a.Location;
+        var accumulator = step;
+        while (b.Location + accumulator is { } next && next.X >= 0 && next.X <= bounds.X && next.Y >= 0 && next.Y <= bounds.Y)
+        {
+            accumulator += step;
+            yield return next;
+        }
+        step = a.Location - b.Location;
+        accumulator = step;
+        while (a.Location + accumulator is { } next && next.X >= 0 && next.X <= bounds.X && next.Y >= 0 && next.Y <= bounds.Y)
+        {
+            accumulator += step;
+            yield return next;
+        }
+    }
+
+    private sealed record Antenna(Vector2 Location, char Frequency)
+    {
+
+    }
+
+    private sealed record AntiNodes(Vector2 Location, HashSet<char> Frequencies)
+    {
+        public bool IsCancelledOut => false;
     }
 }
