3a00d8a9511478f07612b1d21753b86497d3d085
adam.beseler <adam.beseler@gmail.com>
2024-12-08 16:11:55 -0600

    solver cleanup

--- Common/PuzzleSolver.cs
+++ Common/PuzzleSolver.cs
@@ -2,55 +2,61 @@
 
 public static class PuzzleSolver
 {
-    private static readonly ConsoleColor[] colors = [
-        ConsoleColor.Green,
-        ConsoleColor.Red,
-    ];
-    private static int colorIndex = 0;
-
     public static void SolveAll(Type type)
     {
-        var puzzleSolutions = type.Assembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(PuzzleSolution))).OrderBy(t => t.Name);
+        ReadOnlySpan<ConsoleColor> colors = [ConsoleColor.Green, ConsoleColor.Red];
+        var colorIndex = 0;
+
+        var puzzleSolutions = type.Assembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(PuzzleSolution))).OrderBy(t => t.Name);        
         foreach (var puzzleSolution in puzzleSolutions)
         {
-            try
-            {
-                typeof(PuzzleSolver)
-                    .GetMethods()
-                    .FirstOrDefault(m => m.Name == nameof(PuzzleSolution.Solve) && m.IsGenericMethod)?
-                    .MakeGenericMethod(puzzleSolution)
-                    .Invoke(null, null);
+            var result = typeof(PuzzleSolver)
+                .GetMethods()
+                .FirstOrDefault(m => m.Name == nameof(Solve) && m.IsGenericMethod)?
+                .MakeGenericMethod(puzzleSolution)
+                .Invoke(null, null);
 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error while solving {puzzleSolution.Name}: {e.Message}");
-            }
+            Console.ForegroundColor = colors[colorIndex];
+            Console.WriteLine(result);
+
+            colorIndex = (colorIndex + 1) % colors.Length;
         }
-
         Console.ResetColor();
     }
 
-    public static void Solve<T>() where T : PuzzleSolution
+    public static string Solve<T>() where T : PuzzleSolution
     {
-        Console.ForegroundColor = colors[colorIndex];
-        colorIndex = (colorIndex + 1) % colors.Length;
-        var result = Solve<T>(T.TestFileName);
-
-        if (string.IsNullOrEmpty(result))
+        try
         {
-            return;
+            var result = Solve<T>(T.TestFileName);
+            if (string.IsNullOrEmpty(result)) return result;
+            if (result != T.TestOutputExpected)
+            {
+                return $"{typeof(T).Name} - Test failed. Expected ({T.TestOutputExpected}) but got ({result})";
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            return $"{typeof(T).Name} - No test file found. Skipping test.";
+        }
+        catch (Exception e)
+        {
+            return $"{typeof(T).Name} - Test failed. {e.Message}";
         }
 
-        if (result != T.TestOutputExpected)
+        try
         {
-            Console.WriteLine($"{typeof(T).Name} - Test failed. Expected ({T.TestOutputExpected}) but got ({result})");
-            return;
+            var result = Solve<T>(T.FileName);
+            return $"{typeof(T).Name} - {result}";
         }
-
-        result = Solve<T>(T.FileName);
-
-        Console.WriteLine($"{typeof(T).Name} - {result}");
+        catch (FileNotFoundException)
+        {
+            return $"{typeof(T).Name} - No file found.";
+        }
+        catch (Exception e)
+        {
+            return $"{typeof(T).Name} - {e.Message}";
+        }
     }
 
     public static string Solve<T>(string fileName) where T : PuzzleSolution
