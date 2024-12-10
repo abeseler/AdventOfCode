c436b074abdacaa6c3a5a88c1b8ee0ed85e2deaf
adam.beseler <adam.beseler@gmail.com>
2024-12-09 18:37:46 -0600

    day 10 setup

---
+++ 2024/Solutions/Day10_Part2.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode.Solutions;
+
+internal sealed class Day10_Part2 : PuzzleSolution
+{
+    private const string DAY = "10";
+    public static string FileName { get; } = $"Data/{DAY}.input";
+    public static string TestFileName { get; } = $"Data/{DAY}.sample";
+    public static string TestOutputExpected { get; } = "";
+    public static string Solve(StreamReader reader)
+    {
+        throw new NotImplementedException();
+    }
+}
