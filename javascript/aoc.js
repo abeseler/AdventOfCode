fcbccbad11ac875e547555f01ca076b70eb02f36
adam.beseler <adam.beseler@gmail.com>
2024-12-11 20:57:35 -0600

    updated js puzzle runner

--- javascript/aoc.js
+++ javascript/aoc.js
@@ -6,22 +6,38 @@ async function runSolutions() {
     const totalStart = process.hrtime();
     const files = readdirSync(solutionsFolder);
 
+    const results = [];
+
     await Promise.all(files.map(async (file) => {
         const solution = await import(`./${solutionsFolder}/${file}`);
         let content = readFileSync(`${dataFolder}/${solution.testFile}`, 'utf-8');
         let result = solution.solve(content);
 
         if (result !== solution.testOutputExpected) {
-            console.log(`${file} - Test failed. Expected (${solution.testOutputExpected}) but got (${result})`);
+            results.push({file, passed: false, result, expected: solution.testOutputExpected});
         }
         else {
             content = readFileSync(`${dataFolder}/${solution.inputFile}`, 'utf-8');
             let start = process.hrtime();
             result = solution.solve(content);
             let duration = (process.hrtime(start)[1] / 1000000).toFixed(2);
-            console.log(`${file} - ${result} [${duration}ms]`);
+            results.push({file, passed: true, result, duration});
         }
     }));
+
+    results.sort((a, b) => {
+        return a.file.localeCompare(b.file, undefined, {numeric: true, sensitivity: 'base'});
+    });
+
+    console.log('\nResults:');
+    results.forEach(r => {
+        if (!r.passed) {
+            console.log(`${r.file} - Test failed. Expected (${r.expected}) but got (${r.result})`);
+        }
+        else {
+            console.log(`${r.file} - ${r.result} [${r.duration}ms]`);
+        }
+    });
 
     let totalDuration = (process.hrtime(totalStart)[1] / 1000000).toFixed(2);
     console.log(`\nTotal duration: ${totalDuration}ms`);
